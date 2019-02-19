d625dda97749dc92be3fb854a651e31c3b257c41
Richard Camuccio <rcamuccio@gmail.com>
2019-02-19 14:23:03 -0600

    Added astrometry.net and iraf installer files

---
+++ astrometry-installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Richard Camuccio
+# 19 Feb 2019
+#
+
+sudo apt install -y curl libastrometry0 libc6 libcairo2 libgsl23 libjpeg8 libnetpbm10 libpng16-16 netpbm python python-astrometry python-pyfits python3 python3-exif libcfitsio-bin sextractor libatlas3-base libfftw3-single3
+
+sudo apt install -y astrometry.net astrometry-data-2mass astrometry-data-2mass-00 astrometry-data-2mass-01 astrometry-data-2mass-02 astrometry-data-2mass-03 astrometry-data-2mass-04 astrometry-data-2mass-05 astrometry-data-2mass-06 astrometry-data-2mass-07 astrometry-data-2mass-08-19 astrometry-data-tycho2  astrometry-data-tycho2-07  astrometry-data-tycho2-07-bigendian astrometry-data-tycho2-07-littleendian astrometry-data-tycho2-08 astrometry-data-tycho2-08-bigendian astrometry-data-tycho2-08-littleendian astrometry-data-tycho2-09 astrometry-data-tycho2-09-bigendian astrometry-data-tycho2-09-littleendian astrometry-data-tycho2-10-19 astrometry-data-tycho2-10-19-bigendian astrometry-data-tycho2-10-19-littleendian
---
+++ iraf-installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Richard Camuccio
+# 19 Feb 2019
+#
+
+sudo apt install iraf iraf-dev iraf-noao iraf-noao-dev iraf-rvsao iraf-wcstools imview xfig python3-pyraf
